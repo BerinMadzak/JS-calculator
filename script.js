9280db1855675dad069ef967a4b95d7b9146133a
TGS Berin <berinmadzak@gmail.com>
2023-02-02 21:05:13 +0100

    Support for unary operations and constants

--- script.js
+++ script.js
@@ -34,6 +34,103 @@ class Calculator {
         this.currentOperand = ''
     }
 
+    setConstant(constant) {
+        switch (constant) {
+            case 'e':
+                this.currentOperand = 2.7182818284590452353602874713527
+                break
+            case String.fromCharCode(960):
+                this.currentOperand = 3.1415926535897932384626433832795
+            default: return
+        }
+    }
+
+    unaryOperation(operation) {
+        let result
+        let op
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(current)) return
+
+        switch (operation) {
+            case 'sin':
+                result = Math.sin(current)
+                op = `sin(${current})`
+                break
+            case 'cos':
+                result = Math.cos(current)
+                op = `cos(${current})`
+                break
+            case 'tan':
+                result = Math.tan(current)
+                op = `tan(${current})`
+                break
+            case 'asin':
+                if (current < -1 || current > 1) return
+                result = Math.asin(current)
+                op = `asin(${current})`
+                break
+            case 'acos':
+                if (current < -1 || current > 1) return
+                result = Math.acos(current)
+                op = `acos(${current})`
+                break
+            case 'atan':
+                result = Math.atan(current)
+                op = `atan(${current})`
+                break
+            case '1/x':
+                result = 1 / current
+                op = `1/${current}`
+                break
+            case '|x|':
+                result = Math.abs(current)
+                op = `|${current}|`
+                break
+            case '+/-':
+                result = -current
+                op = ''
+                break
+            case 'log':
+                result = Math.log10(current)
+                op = `log(${current})`
+                break
+            case 'ln':
+                result = Math.log(current)
+                op = `ln(${current})`
+                break
+            case 'x2':
+                result = Math.pow(current, 2)
+                op = `sqr(${current})`
+                break
+            case 'x3':
+                result = Math.pow(current, 3)
+                op = `cube(${current})`
+                break
+            case String.fromCharCode(8730) + 'x':
+                result = Math.sqrt(current)
+                op = `${String.fromCharCode(8730)}(${current})`
+                break
+            case String.fromCharCode(8731) + 'x':
+                result = Math.cbrt(current)
+                op = `${String.fromCharCode(8731)}(${current})`
+                break
+            case 'n!':
+                result = this.factoriel(current)
+                op = `fact(${current})`
+                break
+            default: return
+        }
+        this.previousOperand = op
+        this.currentOperand = result
+        this.operation = null
+    }
+
+    factoriel(num) {
+        let sum = 1
+        for (let i = 2; i <= num; i++) sum *= i
+        return sum
+    }
+
     compute() {
         let result
         const previous = parseFloat(this.previousOperand)
@@ -52,6 +149,9 @@ class Calculator {
                 break
             case '/':
                 result = previous / current
+                break
+            case 'sin':
+                result = sin(current)
                 break
             default: return
         }
@@ -78,9 +178,12 @@ class Calculator {
         }
     }
 
-    updateDisplay() {
+    updateDisplay(unary = false) {
         this.currentOperandText.innerText = this.getDisplayNumber(this.currentOperand)
-        if (this.operation != null) {
+        if (unary) {
+            this.previousOperandText.innerText = this.previousOperand
+        }
+        else if (this.operation != null) {
             this.previousOperandText.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
         }
         else {
@@ -91,6 +194,8 @@ class Calculator {
 
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
+const unaryOperationButtons = document.querySelectorAll('[data-operation-unary]')
+const constantButtons = document.querySelectorAll('[data-constant]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
 const clearButton = document.querySelector('[data-clear]')
@@ -109,6 +214,20 @@ numberButtons.forEach(button => {
 operationButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+unaryOperationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.unaryOperation(button.innerText)
+        calculator.updateDisplay(true)
+    })
+})
+
+constantButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.setConstant(button.innerText)
         calculator.updateDisplay()
     })
 })
